5885a4f0b77e16d5b265b7341d6df421e7c48669
shadow-eljan <eljanrai25@gmail.com>
2025-11-28 12:39:23 +0545

    Variable declaration and function

---
+++ day1.js
@@ -0,0 +1,13 @@
+// var x = 10;
+// let y = 15;
+// console.log(x);
+// console.log(`My name is ${x}`);
+
+// function apple(){
+//     return x+y;
+// }
+
+// console.log(apple());
+const sum = (num1, num2) => num1+num2;      
+console.log(sum(1,2))
+
