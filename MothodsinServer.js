8e824aab92bbe36943c038d1687c8234265a37aa
shadow-eljan <eljanrai25@gmail.com>
2025-12-14 14:11:24 +0545

    POST method

--- MothodsinServer.js
+++ MothodsinServer.js
@@ -2,7 +2,7 @@ const http = require("http");
 const url = require("url");
 
 const notes = [
-    {id: 1, title: "Apple", Content: " I love eating apples."},
+    {id: 1, title: "Apple a", Content: " I love eating apples."},
     {id: 2, title: "Banana", Content: "I love eating banana."}
 ];
 
@@ -14,15 +14,30 @@ const server = http.createServer((req,res) => {
     if(method == "GET"){
         if(urlobj.pathname == "/api/notes"){
             const noteId = urlobj.query?.id; // ? needed if there is no object 
+            const titleId = urlobj.query?.title;
             if(noteId){
                 const filterNotes = notes.filter((note) => note.id == noteId)
                 res.end(JSON.stringify(filterNotes));
             }else{
                 res.end(JSON.stringify(notes));
             }
+            // if(titleId){
+            //     const filterTitle = notes.filter((note) => note.title == titleId);
+            //     res.end(JSON.stringify(filterTitle))
+            // }else{
+            //     res.end(JSON.stringify(notes));
+            // }
         }
-    }else{
-        
+    }else if (method == "POST") {
+        if(urlobj.pathname == "/api/notes"){
+            let body = "";
+            req.on("data", (chunk) => body += chunk);
+            req.on("end", ()=> {
+                const data = JSON.parse(body);
+                notes.push({...data, id:notes.length + 1});
+                res.end(JSON.stringify({message: "Node Created."}));
+            });
+        }
     }
 })
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,3 @@
-const {sum, sub} = require("./calculator");
-// sum(1,2);
-// sub(5,2);
+
+
+server.listen(4000, ()=> console.log("Server is up."))
